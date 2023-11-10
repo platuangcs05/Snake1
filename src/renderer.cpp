f43b7b2e5a37c833fe52bb74c7ac1b3f0fb29910
platuangcs05 <89737355+platuangcs05@users.noreply.github.com>
2023-11-10 18:06:36 +0900

    2
    2

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -79,6 +79,11 @@ void Renderer::Render(Snake const snake, SDL_Point const& food, SDL_Point const&
     }
     else {
         SDL_SetRenderDrawColor(sdl_renderer.get(), 0xFF, 0x00, 0x00, 0xFF);
+        //TUAN
+        SDL_Rect temp;
+        temp.w = screen_width;
+        temp.h = screen_height;
+        SDL_RenderFillRect(sdl_renderer.get(), &temp);
     }
     SDL_RenderFillRect(sdl_renderer.get(), &block);
 
