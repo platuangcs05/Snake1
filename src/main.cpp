289b79c48da934799a2bdddc37cb5f75f840c663
platuangcs05 <89737355+platuangcs05@users.noreply.github.com>
2023-11-14 14:46:24 +0900

    1
    1

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
+
 #include "controller.h"
 #include "game.h"
 #include "renderer.h"
-//#include "SnakeLog.h"
 
-#include <chrono>
+//#include "SnakeLog.h"
 
 int main() {
     constexpr std::size_t kFramesPerSecond{60};
@@ -20,16 +22,23 @@ int main() {
     Game game(kGridWidth, kGridHeight);
     //SnakeLog SnakeLog;
     //SnakeLog.Start();
-    auto startTime = std::chrono::steady_clock::now();
+    //auto startTime = std::chrono::steady_clock::now();
+    steady_clock::time_point startTime = steady_clock::now(); // 1. 現在日時を取得
+    std::this_thread::sleep_for(seconds(3)); // 2. 時間のかかる処理...
     
     game.Run(controller, renderer, kMsPerFrame);
-    auto endTime = std::chrono::steady_clock::now();
-    auto elapsedTime = std::chrono::duration_cast<std::chrono::seconds>(endTime - startTime).count();
-    //SnakeLog.End(game.GetScore(), game.GetSize(), elapsedTime);
+
+    //auto endTime = std::chrono::steady_clock::now();
+    steady_clock::time_point endTime = steady_clock::now(); // 3. 現在日時を再度取得 // 1～3の間にシステム時計が変更されても時間が逆行することはない
+
+    //auto elapsedTime = duration_cast<seconds>(endTime - startTime).count();
+    auto snakeTime = duration_cast<seconds>(endTime - startTime).count();
+
+    //SnakeLog.End(game.GetScore(), game.GetSize(), snakeTime);
 
     //[TUAN] Add time
-    std::cout << "[Program completion time]: " << elapsedTime << " seconds\n";
-    std::cout << "[Final score]: " << game.GetScore() << "\n";
-    std::cout << "[Snake size]: " << game.GetSize() << "\n";
+    std::cout << "Snake Time: " << snakeTime << " seconds\n";
+    std::cout << "Snake Score: " << game.GetScore() << "\n";
+    std::cout << "Snake Size: " << game.GetSize() << "\n";
     return 0;
 }
